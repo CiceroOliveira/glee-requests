6098a37184215ba1122315df7ff0b51abcf70f4c
Cicero Oliveira <cicero@cicerooliveira.com>
2011-03-13 13:43:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GleeRequests::Application.config.secret_token = 'b93daae560923dfd2582d38b94183d54279cfa4bf7c3af1b1c29d4d5d05c7d1e52b8574e3e34b6b85cc423a2fbc808ef5274a57dfed750e8c73784d3c6ee471c'
